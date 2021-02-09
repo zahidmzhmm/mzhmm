011c49b00fb6bd4384513cf98b6e88e5bc11c39b
zahidmzhmm <zahidmzhmm@gmail.com>
2021-02-10 00:04:23 +0600

    Personal

--- src/component/about/AboutPage.jsx
+++ src/component/about/AboutPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import classes from "./AboutPage.module.css";
 
 const AboutPage = () => {
     return (
         <>
-
+            <div className="container pb-5 mb-5">
+                <h2 className={`${classes.title_main} pt-4`}><span className={classes.main_title_section}>About Us</span></h2>
+            </div>
         </>
     );
 };
