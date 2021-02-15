1130828f3a6ba694c302bc91f1ff992d31d19034
zahidmzhmm <zahidmzhmm@gmail.com>
2021-02-15 23:55:59 +0600

    Personal

--- admin/php/phpmailer/credential.php
+++ admin/php/phpmailer/credential.php
@@ -2,6 +2,6 @@
 	/*Update credentials*/
 // 	define('EMAIL', 'zahidfromwebsite@gmail.com');
 // 	define('PASS', 'ezrsrnehpptoatdn');
-	define('EMAIL', 'info@zahidmzhmm.com');
+    define('EMAIL', 'no-reply@zahidmzhmm.com');
 	define('PASS', 'm-zhm118899S');
  ?>
